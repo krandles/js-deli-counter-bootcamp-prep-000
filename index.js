6b1c14e80003b077447faab8b57fbfde969ba21f
Kevin Randles <krandles@gmail.com>
2017-10-31 11:49:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  var line = "The line is currently"
+  var line = "The line is currently empty."
   var i = 0
   while (i < katzDeliLine.length) {
     line += ` ${i + 1}. ${katzDeliLine[i]},`
