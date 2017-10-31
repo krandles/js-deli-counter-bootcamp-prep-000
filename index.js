6e3ccdbe5cd2b09fc9605bd903e337480de3109a
Kevin Randles <krandles@gmail.com>
2017-10-31 11:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ var katzDeli =[]
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    return `Currently serving ${katzDeliLine[0]}.`
+    var result = `Currently serving ${katzDeliLine[0]}.`
+    katzDeliLine.shift
+    return result
   }
 }
 
