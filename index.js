006fd291ecbd50b786451d7202849ce2035fe163
Kevin Randles <krandles@gmail.com>
2017-10-31 11:50:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!"
   } else {
     var result = `Currently serving ${katzDeliLine[0]}.`
-    katzDeliLine.shift
+    katzDeliLine.shift()
     return result
   }
 }
